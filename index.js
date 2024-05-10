942221ebd28454455da7cc01a335a71a72a0ff0c
designer-ab <per.useravi0@gmail.com>
2024-05-11 01:13:08 +0600

    added update blog api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,40 @@ async function run() {
       const blogs = req.body;
       // console.log(blogs);
       const result = await blogsCollection.insertOne(blogs);
+      res.send(result);
+    });
+
+    app.get('/blogs', async(req, res) => {
+      const cursor = blogsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/blogs/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id : new ObjectId(id)};
+      const result = await blogsCollection.findOne(query);
+      res.send(result)
+    });
+
+    app.put('/blogsUpdate/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter= {_id: new ObjectId(id)};
+      const options = { upsert: true};
+      const updatedBlog = req.body;
+      console.log(updatedBlog)
+
+      const blog = {
+        $set : {
+          title: updatedBlog.title,
+          category: updatedBlog.category,
+          imageUrl : updatedBlog.imageUrl,
+          shortDes : updatedBlog.shortDes,
+          longDes: updatedBlog.longDes,
+        }
+      }
+      const result = await blogsCollection.updateOne(filter, blog, options);
       res.send(result);
     });
 
